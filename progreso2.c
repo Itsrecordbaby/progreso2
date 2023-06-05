ff0b23079d3f8e56f5aa24dda509ebc819a2b9c4
Mati <amaticc03@gmail.com>
2023-06-05 16:33:44 -0500

    cuarto commit

--- progreso2.c
+++ progreso2.c
@@ -24,26 +24,41 @@ float obtenerPromedioGrupo(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS]) {
     }
     return suma / (NUM_ALUMNOS * NUM_PROGRESOS);
 
-int Mejoralumno
-float obtenerPromedioGrupo(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS]) {
-    float suma = 0.0;
+int main() {
+    float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS];
+    float promedios[NUM_ALUMNOS];
+
+    srand(time(NULL)); //Generador de números aleatorios
+
+// Generar notas aleatorias para cada estudiante y cada progreso
     for (int i = 0; i < NUM_ALUMNOS; i++) {
         for (int j = 0; j < NUM_PROGRESOS; j++) {
-            suma += calificaciones[i][j];
+            calificaciones[i][j] = (float)(rand() % 10 + 1); // Generar un número aleatorio entre 1 y 10
         }
     }
-    return suma / (NUM_ALUMNOS * NUM_PROGRESOS);
+    //Se realizo una tabla de calificacion para mostrar los numeros random
+printf("Tabla de calificaciones:\n\n");
+    printf("Estudiante\tProgreso 1\tProgreso 2\tProgreso 3\n");
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        printf("%d\t\t", i + 1);
+        for (int j = 0; j < NUM_PROGRESOS; j++) {
+            printf("%.2f\t\t", calificaciones[i][j]);
+        }
+        printf("\n");
+    }
 
+    printf("\nPromedio de calificaciones de cada alumno:\n");
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        printf("Alumno %d: %.2f\n", i + 1, promedios[i]);
+    }
 
+    printf("\nPromedio del grupo para cada progreso: %.2f\n", promedioGrupo);
 
+    printf("\nAlumno con el mayor promedio de calificación:\n");
+    printf("Alumno %d\n", mejorAlumno + 1);
 
-
+    return 0;
 }
-
-
-
-
-
 
 
 
