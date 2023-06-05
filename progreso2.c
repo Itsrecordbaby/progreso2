39a1cf73800ac2daa858128285cb5162c2e21b4e
Mati <amaticc03@gmail.com>
2023-06-05 16:01:39 -0500

    Segundo commit

--- progreso2.c
+++ progreso2.c
@@ -4,7 +4,7 @@
 
 #define NUM_ALUMNOS 23
 #define NUM_PROGRESOS 3
-
+//Esto sirve para obtener el promedio de calificaciones de cada alumno
 void obtenerPromedioAlumnos(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS], float promedios[NUM_ALUMNOS]) {
     for (int i = 0; i < NUM_ALUMNOS; i++) {
         float suma = 0.0;
@@ -14,15 +14,37 @@ void obtenerPromedioAlumnos(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS], fl
         promedios[i] = suma / NUM_PROGRESOS;
     }
 }
+//Esto sirve para obtener el promedio del grupo
+float obtenerPromedioGrupo(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS]) {
+    float suma = 0.0;
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        for (int j = 0; j < NUM_PROGRESOS; j++) {
+            suma += calificaciones[i][j];
+        }
+    }
+    return suma / (NUM_ALUMNOS * NUM_PROGRESOS);
 
-int main() {
-
-
-
+int Mejoralumno
+float obtenerPromedioGrupo(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS]) {
+    float suma = 0.0;
+    for (int i = 0; i < NUM_ALUMNOS; i++) {
+        for (int j = 0; j < NUM_PROGRESOS; j++) {
+            suma += calificaciones[i][j];
+        }
+    }
+    return suma / (NUM_ALUMNOS * NUM_PROGRESOS);
 
 
 
 
 
 }
+
+
+
+
+
+
+
+
    
